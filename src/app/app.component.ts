2aa041840f1a79912e59c81d9f5fa09a141f30bd
Ádám Sipos <adam2458hu@gmail.com>
2021-10-27 17:12:09 +0200

    az egyes aloldalak címének fordítása ideiglenesen ide lett elhelyezve

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -62,34 +62,3 @@ export class AppComponent implements OnInit{
       });
   }
 }
-
-/*var cart:any=[];
-
-function getParam(param:string) {
-  return new URLSearchParams(window.location.search).get(param);
-}
-
-function getParam2(param:string){
-  if (window.location.search) {
-    let params={};
-
-    paramArray = window.location.search.substring(1).split('&');
-    for(i=0;i<paramArray.length;i++){
-      let currentParam = paramArray[i].split('=');
-      params[currentParam[0]]=currentParam[1];
-    }
-    return params;
-  }
-
-  return false;
-}
-
-function loadCart(){
-  if (localStorage.getItem("cart")) cart = JSON.parse(localStorage.getItem("cart"));
-  document.getElementById("item-count").innerHTML=cart.length?` (${cart.length})`:"";
-  console.log(cart);
-}
-
-window.onload = function(){
-  loadCart();
-}*/
