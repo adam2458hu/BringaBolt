58012896ee3ef9570953908f6004b54583914f31
Ádám Sipos <adam2458hu@gmail.com>
2021-10-27 21:28:16 +0200

    a termékek lekérdezéséhez szükséges végpontok környezeti változón keresztüli elérése

--- src/app/core/product.service.ts
+++ src/app/core/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,6 @@ export class ProductService {
   constructor(private http:HttpClient) { }
 
   fetchProducts(filters: any) {
-    return this.http.get(`http://localhost:3000/api/products/`,{params: filters});
+    return this.http.get(environment.apiProductsURL,{params: filters});
   }
 }
