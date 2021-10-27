32bcf3df1498e9b494b7df3a281a1ade16260c18
Ádám Sipos <adam2458hu@gmail.com>
2021-10-27 18:49:48 +0200

    felkészítés herokuhoz

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const productsRouter = require('./routes/products');
+const path = require('path');
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(__dirname+'/dist/BringaBolt/',{dotFiles: 'allow'}));
+app.use(express.static(__dirname+'/dist/BringaBolt'));
 app.use('/api/products',productsRouter);
 
 app.get('*', function (req, res){
-    res.sendFile(__dirname+'/dist/BringaBolt/index.html');
+    res.sendFile(path.join(__dirname+'/dist/BringaBolt/index.html'));
 });
 
 app.listen(port,()=>{
