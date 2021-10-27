254ecd257a40411a1b0e45ccd84455c7b2ff7601
Ádám Sipos <adam2458hu@gmail.com>
2021-10-27 18:03:34 +0200

    végpont hozzáadása a statikus fájlok kiszolgálásához

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(__dirname+'/dist/BringaBolt/',{dotFiles: 'allow'}));
 app.use('/api/products',productsRouter);
+
+app.get('*', function (req, res){
+    res.sendFile(__dirname+'/dist/BringaBolt/index.html');
+});
 
 app.listen(port,()=>{
 	console.log(`A szerver fut a ${port} porton.`);
