e3438a03fe71ea31dafd759f3a45bf2f4152e1b3
Ádám Sipos <adam2458hu@gmail.com>
2021-10-27 17:15:02 +0200

    az termék végpontok egyszerűsítése

--- src/app/core/product.service.ts
+++ src/app/core/product.service.ts
@@ -9,31 +9,7 @@ export class ProductService {
 
   constructor(private http:HttpClient) { }
 
-  fetchProducts() {
-    return this.http.get(`http://localhost:3000/api/products/`);
-  }
-
-  fetchProductsByName(name:any){
-    return this.http.get(`http://localhost:3000/api/products/getProductsByName/${name}`);
-  }
-  
-  fetchProductsByCategory(filter:any){
-    return this.http.get('http://localhost:3000/api/products/getProductsByCategory',{params:filter});
-  }
-
-  fetchProductBySlug(slug:string){
-    return this.http.get(`http://localhost:3000/api/products/getProductBySlug/${slug}`);
-  }
-
-  fetchProductImages(id: number,type: string,color: string){
-    return this.http.get(`http://localhost:3000/api/products/getProductImages/${id}/${type}/${color}`);
-  }
-
-  fetchProducts360degreeImagesCount(slug:string){
-    return this.http.get(`http://localhost:3000/api/products/getProduct360degreeImagesCount/${slug}`);
-  }
-
-  fetchProductById(id:string){
-    return this.http.get(`http://localhost:3000/api/products/getProductById/${id}`);
+  fetchProducts(filters: any) {
+    return this.http.get(`http://localhost:3000/api/products/`,{params: filters});
   }
 }
